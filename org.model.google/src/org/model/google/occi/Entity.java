a5997982f9e4f0467798e80ee204ca5b921d2833
Stephanie Challita <schallit@ad.inria.fr>
2017-06-01 11:15:03 +0200

    Initial commit of GCP project

---
+++ org.model.google/src/org/model/google/occi/Entity.java
@@ -0,0 +1,174 @@
+/**
+ */
+package org.model.google.occi;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Entity</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * </p>
+ * <ul>
+ *   <li>{@link org.model.google.occi.Entity#getId <em>Id</em>}</li>
+ *   <li>{@link org.model.google.occi.Entity#getTitle <em>Title</em>}</li>
+ *   <li>{@link org.model.google.occi.Entity#getKind <em>Kind</em>}</li>
+ *   <li>{@link org.model.google.occi.Entity#getMixins <em>Mixins</em>}</li>
+ *   <li>{@link org.model.google.occi.Entity#getAttributes <em>Attributes</em>}</li>
+ * </ul>
+ *
+ * @see org.model.google.occi.OcciPackage#getEntity()
+ * @model abstract="true"
+ *        annotation="http://www.eclipse.org/emf/2002/Ecore constraints='IdUnique AttributesNameUnique KindCompatibleWithOneAppliesOfEachMixin'"
+ *        annotation="http://www.eclipse.org/emf/2002/Ecore/OCL/Pivot IdUnique='Entity.allInstances()->isUnique(id)' AttributesNameUnique='attributes->isUnique(name)' KindCompatibleWithOneAppliesOfEachMixin='mixins->forAll(m | m.applies->notEmpty() implies m.applies->exists(k | kind->closure(parent)->includes(k)))'"
+ * @generated
+ */
+public interface Entity extends EObject {
+	/**
+	 * Returns the value of the '<em><b>Id</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Id</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Id</em>' attribute.
+	 * @see #setId(String)
+	 * @see org.model.google.occi.OcciPackage#getEntity_Id()
+	 * @model dataType="org.model.google.occi.URI" required="true"
+	 * @generated
+	 */
+	String getId();
+
+	/**
+	 * Sets the value of the '{@link org.model.google.occi.Entity#getId <em>Id</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Id</em>' attribute.
+	 * @see #getId()
+	 * @generated
+	 */
+	void setId(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Title</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Title</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Title</em>' attribute.
+	 * @see #setTitle(String)
+	 * @see org.model.google.occi.OcciPackage#getEntity_Title()
+	 * @model dataType="org.model.google.occi.String"
+	 * @generated
+	 */
+	String getTitle();
+
+	/**
+	 * Sets the value of the '{@link org.model.google.occi.Entity#getTitle <em>Title</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Title</em>' attribute.
+	 * @see #getTitle()
+	 * @generated
+	 */
+	void setTitle(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Kind</b></em>' reference.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Kind</em>' reference isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Kind</em>' reference.
+	 * @see #setKind(Kind)
+	 * @see org.model.google.occi.OcciPackage#getEntity_Kind()
+	 * @model required="true"
+	 * @generated
+	 */
+	Kind getKind();
+
+	/**
+	 * Sets the value of the '{@link org.model.google.occi.Entity#getKind <em>Kind</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Kind</em>' reference.
+	 * @see #getKind()
+	 * @generated
+	 */
+	void setKind(Kind value);
+
+	/**
+	 * Returns the value of the '<em><b>Mixins</b></em>' reference list.
+	 * The list contents are of type {@link org.model.google.occi.Mixin}.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Mixins</em>' reference list isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Mixins</em>' reference list.
+	 * @see org.model.google.occi.OcciPackage#getEntity_Mixins()
+	 * @model
+	 * @generated
+	 */
+	EList<Mixin> getMixins();
+
+	/**
+	 * Returns the value of the '<em><b>Attributes</b></em>' containment reference list.
+	 * The list contents are of type {@link org.model.google.occi.AttributeState}.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Attributes</em>' containment reference list isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Attributes</em>' containment reference list.
+	 * @see org.model.google.occi.OcciPackage#getEntity_Attributes()
+	 * @model containment="true"
+	 * @generated
+	 */
+	EList<AttributeState> getAttributes();
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @model
+	 * @generated
+	 */
+	void occiCreate();
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @model
+	 * @generated
+	 */
+	void occiRetrieve();
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @model
+	 * @generated
+	 */
+	void occiUpdate();
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @model
+	 * @generated
+	 */
+	void occiDelete();
+
+} // Entity
---
+++ org.model.google/src/org/model/google/occi/impl/ActionImpl.java
@@ -0,0 +1,37 @@
+/**
+ */
+package org.model.google.occi.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.model.google.occi.Action;
+import org.model.google.occi.OcciPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Action</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class ActionImpl extends CategoryImpl implements Action {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ActionImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return OcciPackage.Literals.ACTION;
+	}
+
+} //ActionImpl
