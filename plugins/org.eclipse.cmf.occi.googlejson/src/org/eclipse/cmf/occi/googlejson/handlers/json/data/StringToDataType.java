7376eac645796c58b9449465fc4f77fb41020826
Stephanie Challita <stephanie.challita@inria.fr>
2017-12-19 23:42:11 +0100

    Support arrays and java.lang.Object

--- plugins/org.eclipse.cmf.occi.googlejson/src/org/eclipse/cmf/occi/googlejson/handlers/json/data/StringToDataType.java
+++ plugins/org.eclipse.cmf.occi.googlejson/src/org/eclipse/cmf/occi/googlejson/handlers/json/data/StringToDataType.java
@@ -28,9 +28,27 @@ public class StringToDataType {
 	public final static NumericType typeModelShort = OCCIFactory.eINSTANCE.createNumericType();
 	
 	public final static EObjectType typeModelMap = OCCIFactory.eINSTANCE.createEObjectType();
-	
+	public final static ArrayType typeModelArray = OCCIFactory.eINSTANCE.createArrayType(); // TODO
+	public final static EObjectType typeModelObject = OCCIFactory.eINSTANCE.createEObjectType();
 	public final static Map<String, DataType> map = new HashMap<>();
 
+	public static void initMap() {
+		map.clear();
+		map.put("string", typeModelString);
+		map.put("boolean", typeModelBoolean);	
+		map.put("byte", typeModelByte);
+		map.put("double", typeModelDouble);
+		map.put("float", typeModelFloat);
+		map.put("integer", typeModelInteger);
+		map.put("number", typeModelInteger);
+		map.put("uint32", typeModelInteger);
+		map.put("long", typeModelLong);
+		map.put("short", typeModelShort);
+		map.put("map", typeModelMap);
+		map.put("array", typeModelArray);
+		map.put("object", typeModelObject);
+	}
+	
 	static {
 		typeModelByte.setType(NumericTypeEnum.BYTE);
 		typeModelByte.setName("byte");
@@ -47,16 +65,11 @@ public class StringToDataType {
 		typeModelBoolean.setName("boolean");
 		typeModelString.setName("string");
 		typeModelMap.setName("map");
-		map.put("string", typeModelString);
-		map.put("boolean", typeModelBoolean);	
-		map.put("byte", typeModelByte);
-		map.put("double", typeModelDouble);
-		map.put("float", typeModelFloat);
-		map.put("integer", typeModelInteger);
-		map.put("number", typeModelInteger);
-		map.put("uint32", typeModelInteger);
-		map.put("long", typeModelLong);
-		map.put("short", typeModelShort);
-		map.put("map", typeModelMap);
+		typeModelMap.setInstanceClassName("java.util.Map");
+		typeModelArray.setName("array");
+		typeModelArray.setType(typeModelObject);
+		typeModelObject.setName("object");
+		typeModelObject.setInstanceClassName("java.lang.Object");
+		initMap();
 	}
 }
